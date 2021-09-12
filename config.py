33a22a8f52941ac8af8bc037e6ebdd29848eed78
shalin <shalin.rono@student.moringaschool.com>
2021-09-12 19:39:33 +0300

    add child classes on class config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     '''
-    General configuration parent class
+    parent class configuration
     '''
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
@@ -22,7 +22,7 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     '''
-    Development  configuration child class
+    Child class development configuration
     Args:
         Config: The parent configuration class with General configuration settings
     '''
