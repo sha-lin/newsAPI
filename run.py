3f849dbd8ed0c084dc1bfcfa51b976ae555553e5
Shalin Rono <86775657+sha-lin@users.noreply.github.com>
2025-09-02 23:37:48 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
 from app import create_app
 
-# Set the configuration environment
-config_name = os.getenv('FLASK_ENV', 'production')
+# Explicitly set the config name
+config_name = 'production'
 
-# Create application instance
+# Create application instance  
 app = create_app(config_name)
+
+# Make sure config is loaded
+if app.config.get('CAT_API_URL') is None:
+    app.config['CAT_API_URL'] = 'https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
 if __name__ == '__main__':
     app.run()
