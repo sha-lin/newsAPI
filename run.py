1296152d247291ef0eaf6c31f9ec32c265797f9e
Shalin Rono <86775657+sha-lin@users.noreply.github.com>
2025-09-02 23:30:55 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 from app import create_app
 
+# Set the configuration environment
+config_name = os.getenv('FLASK_ENV', 'production')
+
 # Create application instance
-app = create_app(os.getenv('FLASK_ENV') or 'production')
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
